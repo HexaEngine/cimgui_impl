6fa7f533e0eed0ca9bfd30355bde165c109e8860
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2024-10-12 14:24:39 +0200

    Fixed wrong path.

--- cimgui_impl.cpp
+++ cimgui_impl.cpp
@@ -7,11 +7,11 @@
 #error "IMGUI_FREETYPE should not be defined without freetype generated cimgui"
 #endif
 #endif
-#include "../imgui/imgui.h"
+#include "imgui/imgui.h"
 #ifdef IMGUI_ENABLE_FREETYPE
 #include "./imgui/misc/freetype/imgui_freetype.h"
 #endif
-#include "../imgui/imgui_internal.h"
+#include "imgui/imgui_internal.h"
 #include "cimgui_impl.h"
 
 CIMGUI_API void igSetCurrentContext(ImGuiContext *ctx)
