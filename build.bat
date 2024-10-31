b21d9980e22f6559a7198fa603af8963e6351702
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2024-10-31 22:43:46 +0100

    Update cmake list.

--- build.bat
+++ build.bat
@@ -8,16 +8,16 @@ mkdir sdl2
 cd ..
 
 rm -r .\build\
-cmake -DCMAKE_TOOLCHAIN_FILE="C:/dev/vcpkg/scripts/buildsystems/vcpkg.cmake" -B build -S . -A arm64 -DBACKENDS_WIN32=OFF -DBACKENDS_NO_GRAPHICS=ON -DBACKENDS_GLFW=OFF -DBACKENDS_SDL2=ON -DIMGUI_WCHAR32=ON
+cmake -DCMAKE_TOOLCHAIN_FILE="C:/dev/vcpkg/scripts/buildsystems/vcpkg.cmake" -B build -S . -A arm64 -DBACKENDS_BASE_ONLY=ON -DIMGUI_WCHAR32=ON
+cmake --build .\build\ --config Release
+mv ./build/Release/*.dll ./bin/base/
+
+rm -r .\build\
+cmake -DCMAKE_TOOLCHAIN_FILE="C:/dev/vcpkg/scripts/buildsystems/vcpkg.cmake" -B build -S . -A arm64 -DBACKENDS_SDL2_ONLY=ON -DIMGUI_WCHAR32=ON
 cmake --build .\build\ --config Release
 mv ./build/Release/*.dll ./bin/sdl2/
 
 rm -r .\build\
-cmake -DCMAKE_TOOLCHAIN_FILE="C:/dev/vcpkg/scripts/buildsystems/vcpkg.cmake" -B build -S . -A arm64 -DBACKENDS_WIN32=OFF -DBACKENDS_NO_GRAPHICS=ON -DBACKENDS_GLFW=ON -DBACKENDS_SDL2=OFF -DIMGUI_WCHAR32=ON
+cmake -DCMAKE_TOOLCHAIN_FILE="C:/dev/vcpkg/scripts/buildsystems/vcpkg.cmake" -B build -S . -A arm64 -DBACKENDS_GLFW_ONLY=ON -DIMGUI_WCHAR32=ON
 cmake --build .\build\ --config Release
 mv ./build/Release/*.dll ./bin/glfw/
-
-rm -r .\build\
-cmake -DCMAKE_TOOLCHAIN_FILE="C:/dev/vcpkg/scripts/buildsystems/vcpkg.cmake" -B build -S . -A arm64 -DBACKENDS_GLFW=OFF -DBACKENDS_SDL2=OFF -DIMGUI_WCHAR32=ON
-cmake --build .\build\ --config Release
-mv ./build/Release/*.dll ./bin/base/
