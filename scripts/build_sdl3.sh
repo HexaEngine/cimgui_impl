4c6e2148ccd4f075c4e75e7a5154caa8c5b55ec6
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2025-07-12 02:56:53 +0200

    Refactor build script to remove redundant variables and streamline directory usage

--- scripts/build_sdl3.sh
+++ scripts/build_sdl3.sh
@@ -7,31 +7,25 @@ ANDROID_API="${GHA_ANDROID_API}"
 ANDROID_NDK="${GHA_ANDROID_NDK}"
 ANDROID_CMAKE_FLAGS="${GHA_ANDROID_CMAKE_FLAGS}"
 
-SRC_DIR="src"
-SDL_DIR="SDL"
-INSTALL_DIR="install"
-BUILD_DIR="build"
-
-cd "$SRC_DIR"
-git clone --recursive https://github.com/libsdl-org/SDL.git $SDL_DIR
-cd "$SDL_DIR"
+cd src
+git clone --recursive https://github.com/libsdl-org/SDL.git SDL
+cd SDL
 git checkout release-3.2.10
 
 CMAKE_ARGS="-DSDL_STATIC=OFF -DSDL_SHARED=ON -DSDL_TEST=OFF -DCMAKE_BUILD_TYPE=Release"
 
 if [[ "$PLATFORM" == "android" ]]; then
-    cmake -S . -B "$BUILD_DIR" $ANDROID_CMAKE_FLAGS -DCMAKE_ANDROID_ARCH_ABI=$ARCH -DCMAKE_ANDROID_API=$ANDROID_API \
-        $CMAKE_ARGS
+    cmake -S . -B ./build $ANDROID_CMAKE_FLAGS -DCMAKE_ANDROID_ARCH_ABI=$ARCH -DCMAKE_ANDROID_API=$ANDROID_API $CMAKE_ARGS
 elif [[ "$PLATFORM" == "linux" ]]; then
-    cmake -S . -B "$BUILD_DIR" $CMAKE_ARGS
+    cmake -S . -B ./build $CMAKE_ARGS
 elif [[ "$PLATFORM" == "macos" ]]; then
-    cmake -S . -B "$BUILD_DIR" -DCMAKE_OSX_ARCHITECTURES="$ARCH" $CMAKE_ARGS
+    cmake -S . -B ./build -DCMAKE_OSX_ARCHITECTURES="$ARCH" $CMAKE_ARGS
 elif [[ "$PLATFORM" == "windows" ]]; then
-    cmake -S . -B "$BUILD_DIR" -A "$ARCH" $CMAKE_ARGS -DCMAKE_SYSTEM_VERSION=10.0.26100.0
+    cmake -S . -B ./build -A "$ARCH" $CMAKE_ARGS -DCMAKE_SYSTEM_VERSION=10.0.26100.0
 else
     echo "Unknown platform: $PLATFORM"
     exit 1
 fi
 
-cmake --build "$BUILD_DIR" --config Release
-cmake --install "$BUILD_DIR" --config Release --prefix "$INSTALL_DIR"
+cmake --build ./build --config Release
+cmake --install ./build --config Release --prefix install
