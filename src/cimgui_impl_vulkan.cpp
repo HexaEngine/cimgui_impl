8dc264033598cecbcf174e91fc9a1dfa4967aec0
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2025-07-14 14:29:31 +0200

    Reorganize Vulkan implementation functions for improved readability and consistency

--- src/cimgui_impl_vulkan.cpp
+++ src/cimgui_impl_vulkan.cpp
@@ -1,4 +1,5 @@
 #include "cimgui_impl_vulkan.h"
+
 CIMGUI_API bool CImGui_ImplVulkan_Init(ImGui_ImplVulkan_InitInfo *info)
 {
     return ImGui_ImplVulkan_Init(info);
@@ -15,13 +16,14 @@ CIMGUI_API void CImGui_ImplVulkan_RenderDrawData(ImDrawData *draw_data, VkComman
 {
     ImGui_ImplVulkan_RenderDrawData(draw_data, command_buffer, pipeline);
 }
-CIMGUI_API void CImGui_ImplVulkan_UpdateTexture(ImTextureData* tex)
-{
-    ImGui_ImplVulkan_UpdateTexture(tex);
-}
 CIMGUI_API void CImGui_ImplVulkan_SetMinImageCount(uint32_t min_image_count)
 {
     ImGui_ImplVulkan_SetMinImageCount(min_image_count);
+}
+
+CIMGUI_API void CImGui_ImplVulkan_UpdateTexture(ImTextureData* tex)
+{
+    ImGui_ImplVulkan_UpdateTexture(tex);
 }
 CIMGUI_API VkDescriptorSet CImGui_ImplVulkan_AddTexture(VkSampler sampler, VkImageView image_view, VkImageLayout image_layout)
 {
@@ -31,6 +33,14 @@ CIMGUI_API void CImGui_ImplVulkan_RemoveTexture(VkDescriptorSet descriptor_set)
 {
     ImGui_ImplVulkan_RemoveTexture(descriptor_set);
 }
+
+CIMGUI_API bool CImGui_ImplVulkan_LoadFunctions(uint32_t api_version, PFN_vkVoidFunction(*loader_func)(const char* function_name, void* user_data), void* user_data)
+{
+    return ImGui_ImplVulkan_LoadFunctions(api_version, loader_func, user_data);
+}
+
+// -----
+
 CIMGUI_API void CImGui_ImplVulkanH_CreateOrResizeWindow(VkInstance instance, VkPhysicalDevice physical_device, VkDevice device, ImGui_ImplVulkanH_Window *wd, uint32_t queue_family, const VkAllocationCallbacks *allocator, int w, int h, uint32_t min_image_count)
 {
     ImGui_ImplVulkanH_CreateOrResizeWindow(instance, physical_device, device, wd, queue_family, allocator, w, h, min_image_count);
@@ -47,11 +57,15 @@ CIMGUI_API VkPresentModeKHR CImGui_ImplVulkanH_SelectPresentMode(VkPhysicalDevic
 {
     return ImGui_ImplVulkanH_SelectPresentMode(physical_device, surface, request_modes, request_modes_count);
 }
+CIMGUI_API VkPhysicalDevice CImGui_ImplVulkanH_SelectPhysicalDevice(VkInstance instance)
+{
+    return ImGui_ImplVulkanH_SelectPhysicalDevice(instance);
+}
+CIMGUI_API uint32_t CImGui_ImplVulkanH_SelectQueueFamilyIndex(VkPhysicalDevice physical_device)
+{
+    return ImGui_ImplVulkanH_SelectQueueFamilyIndex(physical_device);
+}
 CIMGUI_API int CImGui_ImplVulkanH_GetMinImageCountFromPresentMode(VkPresentModeKHR present_mode)
 {
     return ImGui_ImplVulkanH_GetMinImageCountFromPresentMode(present_mode);
-}
-CIMGUI_API bool CImGui_ImplVulkan_LoadFunctions(uint32_t api_version, PFN_vkVoidFunction(*loader_func)(const char* function_name, void* user_data), void* user_data)
-{
-    return ImGui_ImplVulkan_LoadFunctions(api_version, loader_func, user_data);
 }
